c9b9982fe2c3fa0ceab2f0589e87a45af8ddd487
Alyssa Okoene <alyssaokoenebusiness@gmail.com>
2023-05-02 23:50:51 -0500

    complete functions lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    const hq = 42;
+    return Math.abs(hq - block);
+  }
+  
+  function distanceFromHqInFeet(block) {
+    const blockDistance = distanceFromHqInBlocks(block);
+    return blockDistance * 264;
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    const distance = Math.abs(destination - start);
+    if (distance <= 1) {
+      return 0;
+    } else if (distance <= 4) {
+      return (distance - 1) * 264;
+    } else {
+      return 2000 + (distance - 4) * 264;
+    }
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance > 2500) {
+      return 'cannot travel that far';
+    } else if (distance > 2000) {
+      return 25;
+    } else if (distance > 400) {
+      return (distance - 400) * 0.02;
+    } else {
+      return 0;
+    }
+  }
